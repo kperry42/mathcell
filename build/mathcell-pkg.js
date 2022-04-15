28d24dacc3671e2996e5c4b6c39be2927f61caeb
Kevin Perry <perry@alumni.princeton.edu>
2022-04-15 17:02:18 -0400

    Add exports for remaining functions

--- build/mathcell-pkg.js
+++ build/mathcell-pkg.js
@@ -3540,7 +3540,17 @@ ${x3d}
 
 
 module.exports = {
-  MathCell, interact, graphic, generateId,
-  checkLimits, getVariable, setLimit, evaluate
+  // core
+  MathCell, interact, graphic, generateId, checkLimits, getVariable, setLimit, evaluate,
+  // functions
+  minMax, dataReplacer, dataReviver, linspace, lerp, roundTo, ceilTo, floorTo,
+  normalize, translate, rotate, rotateFromZAxis, getCompleteCode,
+  color, colorFromHue, colorFromArg, colorToHexString, colormap,
+  // march/marching-cubes, march/marching-squares
+  isosurface, isoline, isoband,
+  // plot/functions
+  plot, listPlot, polarPlot, parametric, wireframe, surfaceFromLines, diskFromLines, slopeField,
+  // plot/graphics
+  arrow, text, point, line, box, sphere, ellipsoid, cylinder, cone,
 };
 
