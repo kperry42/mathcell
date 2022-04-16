7f3bba4f765552df15f40e9a63079b59eb43464d
Kevin Perry <perry@alumni.princeton.edu>
2022-04-16 19:13:48 -0400

    Add support for setting rotation origin when animating groups

--- build/mathcell.js
+++ build/mathcell.js
@@ -3133,6 +3133,7 @@ function addSurface( s ) {
       group.name = s.options.group;
       scene.add( group );
     }
+    mesh.position.sub(group.position);
     group.add( mesh );
 
     if ( mesh.userData.rotateOnAxis ) {
@@ -3140,6 +3141,11 @@ function addSurface( s ) {
       group.userData.rotateOnAxis = true;
       group.userData.axis = mesh.userData.axis;
       group.userData.angle = mesh.userData.angle;
+
+      if (s.options.rotationOrigin) {
+        const shift = (new THREE.Vector3(...s.options.rotationOrigin)).sub(group.position);
+        group.traverse(obj => obj.position[obj === group ? 'add' : 'sub'](shift));
+      }
     }
 
   } else scene.add( mesh );
