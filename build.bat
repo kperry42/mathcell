25ac1655e42ecfc5ff44b9a41d24bbf2f6942613
Kevin Perry <perry@alumni.princeton.edu>
2022-04-16 09:36:11 -0400

    Consolidate build files

--- build.bat
+++ build.bat
@@ -1,2 +1,1 @@
-copy /b src\*.js + src\march\*.js + src\plot\*.js + src\render\*.js build\mathcell.js
-copy /b build\mathcell.js + src\package\exports.js build\mathcell-pkg.js
+copy /b src\*.js + src\march\*.js + src\plot\*.js + src\render\*.js + src\package\*.js build\mathcell.js
--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-cat src/*.js src/march/*.js src/plot/*.js src/render/{[0-s]*,threejs-template,threejs,[u-z]*}.js > build/mathcell.js
-cat build/mathcell.js src/package/exports.js > build/mathcell-pkg.js
-
+cat src/*.js src/march/*.js src/plot/*.js src/render/{[0-s]*,threejs-template,threejs,[u-z]*}.js src/package/*.js > build/mathcell.js
